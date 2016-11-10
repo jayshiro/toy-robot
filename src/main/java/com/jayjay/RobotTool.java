be20df065853d85dd52dc2b0184c8e164734d1ca
Jayjay Lumanang <jayjay.lumanang@zoak.solutions>
2016-11-10 03:32:18 +0000

    Stitch all services together inside the tool

--- src/main/java/com/jayjay/RobotTool.java
+++ src/main/java/com/jayjay/RobotTool.java
@@ -1,7 +1,30 @@
 package com.jayjay;
 
+import com.jayjay.model.ToyRobot;
+import com.jayjay.service.CommandReader;
+import com.jayjay.service.CommandReaderImpl;
+import com.jayjay.service.RobotService;
+import com.jayjay.service.RobotServiceImpl;
+
+import java.util.Scanner;
+
 public class RobotTool {
     public static void main (String [] args) {
-        System.out.println("hello world");
+        System.out.println("Welcome to Toy Robot Tool.\n" +
+            "Valid case-sensitive commands are 'PLACE x,y,direction','LEFT','RIGHT','MOVE', and 'REPORT'.\n" +
+            "Type in 'quit' to exit.");
+
+        RobotService robotService = new RobotServiceImpl(new ToyRobot());
+        CommandReader commandReader = new CommandReaderImpl(robotService);
+
+        Scanner sc = new Scanner(System.in);
+        String command = "";
+
+        while(!command.equals("quit")) {
+            command = sc.nextLine();
+            commandReader.handle(command);
+        }
+
+        System.out.println("Program has terminated. Thank you for using Toy Robot Tool.");
     }
 }
