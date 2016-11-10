602e79d905bdabd1c613152fe340b42fb2c2013c
Jayjay Lumanang <jayjay.lumanang@zoak.solutions>
2016-11-10 00:46:06 +0000

    Simplified Direction by removing displayName as it is redundant

--- src/main/java/com/jayjay/model/Direction.java
+++ src/main/java/com/jayjay/model/Direction.java
@@ -1,17 +1,8 @@
 package com.jayjay.model;
 
 public enum Direction {
-    NORTH("North"),
-    WEST("West"),
-    EAST("East"),
-    SOUTH("South");
-
-    private String displayName;
-    Direction(String displayName){
-        this.displayName = displayName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
+    NORTH,
+    WEST,
+    EAST,
+    SOUTH;
 }
