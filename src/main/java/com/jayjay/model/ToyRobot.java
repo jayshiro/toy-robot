3d0b3e0b3832490e42f2fc00ae2e751fc40903cb
Jayjay Lumanang <jayjay.lumanang@zoak.solutions>
2016-11-10 00:46:44 +0000

    Removed class implementation and make ToyRobot a stand-alone model

--- src/main/java/com/jayjay/model/ToyRobot.java
+++ src/main/java/com/jayjay/model/ToyRobot.java
@@ -3,5 +3,41 @@ package com.jayjay.model;
 public class ToyRobot {
     private int x;
     private int y;
+    private Direction direction;
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public void addX(int val) {
+        x += val;
+    }
+
+    public void addY(int val) {
+        y += val;
+    }
+
+    public String toString() {
+        return x + "," + y + "," + direction.name();
+    }
 }
