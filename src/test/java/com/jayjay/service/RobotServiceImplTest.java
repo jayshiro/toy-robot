9e7f0e7051600061dcc5195a5ab126464a6acd48
Jayjay Lumanang <jayjay.lumanang@zoak.solutions>
2016-11-10 01:04:18 +0000

    Added test class for RobotServiceImpl to test the methods

---
+++ src/test/java/com/jayjay/service/RobotServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.jayjay.service;
+
+import com.jayjay.model.Direction;
+import com.jayjay.model.ToyRobot;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RobotServiceImplTest {
+    private RobotService robotService;
+
+    @Before
+    public void setup () {
+        robotService = new RobotServiceImpl(new ToyRobot());
+    }
+
+    @Test
+    public void shouldPlaceRobotInsideArea() {
+        robotService.place(1,4, Direction.EAST);
+        assertEquals(robotService.report(), "1,4,EAST");
+        robotService.place(3,1, Direction.NORTH);
+        assertEquals(robotService.report(), "3,1,NORTH");
+    }
+}
